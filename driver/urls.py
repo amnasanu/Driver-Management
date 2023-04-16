ee4c1d40319641592a18b1fb983d637361f6f36c
amnasanu <amnasanu100@gmail.com>
2023-04-16 02:39:27 -0700

    added bulk-driver create

--- driver/urls.py
+++ driver/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import DriverList, DriverDetail, DriverCreate
+from .views import DriverList, DriverDetail, DriverCreate, DriverBulkCreateView
 
 
 urlpatterns = [
     path('driver', DriverList.as_view(), name='driver-list'),
     path('driver/<int:driver_id>', DriverDetail.as_view(), name='driver-detail'),
     path('driver/',DriverCreate.as_view(), name='create-driver' ),
+    path('drivers',DriverBulkCreateView.as_view(), name='bulk-drivers')
 
 
 
--- driver/views.py
+++ driver/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import Driver
 from rest_framework.response import Response
 from .serializers import DriverSerializer, DriverCreateSerializer
+from rest_framework import status
 
 # Create your views here.
 
@@ -42,3 +43,16 @@ class DriverCreate(generics.CreateAPIView):
     serializer_class = DriverCreateSerializer
 
 
+class DriverBulkCreateView(generics.CreateAPIView):
+    serializer_class = DriverCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+        drivers_data = request.data
+        serializer = self.get_serializer(data=drivers_data, many=True)
+        serializer.is_valid(raise_exception=True)
+        drivers = serializer.save()
+
+        return Response({
+            'status': 'success',
+            'drivers': DriverSerializer(drivers, many=True).data
+        }, status=status.HTTP_201_CREATED)
